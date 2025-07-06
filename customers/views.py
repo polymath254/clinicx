d0dc82e03f052bc8c97f80a406f8717801b3ac69
polymath254 <kailaevans254@gmail.com>
2025-07-06 10:34:26 +0300

    Add JWT authentication to secure the API

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,5 @@
-# customers/views.py
 
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import Customer
 from .serializers import CustomerSerializer
@@ -7,3 +7,4 @@ from .serializers import CustomerSerializer
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import viewsets
 from .models import Order
 from .serializers import OrderSerializer
@@ -5,3 +6,4 @@ from .serializers import OrderSerializer
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
